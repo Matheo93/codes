cc8f14acb3a7527bc1d805627e6fe422e2520182
Matheo93 <Math.be2015@gmail.com>
2024-07-09 16:24:31 +0200

    Add logging and debug Firestore integration

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Votre configuration Firebase en utilisant les variables d'environnement
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -12,7 +11,6 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialiser Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
